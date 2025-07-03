6929ceea1dc61a9d254f71b236095d9c0e5c755d
Paulina Vargas <mariavargas315616@correo.itm.edu.co>
2025-07-02 22:11:48 -0500

    Agregar script de base de datos

---
+++ caso_practico_script.sql
@@ -0,0 +1,62 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.4.32-MariaDB : Database - caso_practico_empleados
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`caso_practico_empleados` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `caso_practico_empleados`;
+
+/*Table structure for table `__efmigrationshistory` */
+
+DROP TABLE IF EXISTS `__efmigrationshistory`;
+
+CREATE TABLE `__efmigrationshistory` (
+  `MigrationId` varchar(150) NOT NULL,
+  `ProductVersion` varchar(32) NOT NULL,
+  PRIMARY KEY (`MigrationId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `__efmigrationshistory` */
+
+LOCK TABLES `__efmigrationshistory` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `empleados` */
+
+DROP TABLE IF EXISTS `empleados`;
+
+CREATE TABLE `empleados` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `cargo` varchar(50) DEFAULT NULL,
+  `departamento` varchar(50) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `fechaIngreso` date NOT NULL,
+  `activo` tinyint(1) DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `empleados` */
+
+LOCK TABLES `empleados` WRITE;
+
+insert  into `empleados`(`id`,`nombre`,`correo`,`cargo`,`departamento`,`telefono`,`fechaIngreso`,`activo`) values (1,'Paulina','pvargas@example.com','Desarrollo de software','Sistemas','3117092150','2025-07-02',1),(2,'Pepito','pepito@example.com','Dsllo Software','Sistemas','3114565458','2025-07-01',1),(4,'Daniel','daniel@gmail.com','Software','Sistemas','3114525652','2025-07-03',1);
+
+UNLOCK TABLES;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
